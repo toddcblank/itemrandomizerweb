8113d5c787c9b209f663c37817d02b48a2804b85
Todd Blank <toddcblank@gmail.com>
2019-11-24 09:57:44 -0600

    different buildpack for heroku

--- Program.fs
+++ Program.fs
@@ -1,15 +1,15 @@
 ﻿// Learn more about F# at http://fsharp.org
 
-// open System
-// open Suave
-// open App
+open System
+open Suave
+open App
 
-// let hostName = System.Net.IPAddress.Parse "0.0.0.0"
+let hostName = System.Net.IPAddress.Parse "0.0.0.0"
 
-// DotLiquid.setTemplatesDir (sprintf "%s/templates/" __SOURCE_DIRECTORY__)
-// DotLiquid.setCSharpNamingConvention()
+DotLiquid.setTemplatesDir (sprintf "%s/templates/" __SOURCE_DIRECTORY__)
+DotLiquid.setCSharpNamingConvention()
 
-// [<EntryPoint>]
-// let main argv =
-//     startWebServer { defaultConfig with bindings = [ HttpBinding.create HTTP hostName 8888us ] } App.Router
-//     0 // return an integer exit code
+[<EntryPoint>]
+let main argv =
+    startWebServer { defaultConfig with bindings = [ HttpBinding.create HTTP hostName 80us ] } App.Router
+    0 // return an integer exit code
